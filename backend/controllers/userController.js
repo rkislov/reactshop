af43ec238f4da66837349e18455668ff5db5a743
Роман Кислов <roman@kislovs.ru>
2022-11-29 17:33:18 +0500

    users backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,10 +25,10 @@ export const authUser = asyncHandler(async (req, res) => {
         throw new Error('неправильное имя пользователя или пароль')
     }
     
-    res.send({
-        email, 
-        password,
-    })
+    // res.send({
+    //     email, 
+    //     password,
+    // })
 })
 
 
@@ -39,9 +39,48 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
 
     if(user){
-
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
     } else {
-        res.
+        res.status(404)
+        throw new Error('Пользователь не найден')
     }
 })
 
+
+// @description Register new user
+// @route POST /api/users/login
+// @access Public route
+export const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('Пользователь уже существует')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('Неправильные данные')
+    }
+})
